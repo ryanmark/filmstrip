c15f32fd79ee461f723e3ca6ef946603cc786989
Ryan Mark <ryan@mrk.cc>
2015-04-09 11:34:34 -0400

    include libs, remove middleman branding, framing in all.js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+(function($) {
+  // Browser-side applications do not use the API secret.
+  var client = new Dropbox.Client({ key: "v0wp7w9d9w6c2e2" }),
+      tmpl = _.template($('#embedTemplate').html());
+
+  var renderEmbed = function(data) {
+    return tmpl(data);
+  };
+})(jQuery);
