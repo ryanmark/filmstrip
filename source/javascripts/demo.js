304504e803eed45abec529acde58c5ee8da9b34e
nicolezhu <nzhu94@gmail.com>
2015-04-09 15:13:24 -0400

    dynamically calculate frame duration

--- source/javascripts/demo.js
+++ source/javascripts/demo.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
-	console.log('test');
+	$('#add-img').on('click', function() {
+		$('.image-upload').append('<input type="text"><br>');
+	});
 
-	// gif.render();
-	function setup_css_animations() {
-	    var prefixes = [ '-webkit-', '-moz-', '-o-', '' ];
-	    var keyframes = '';
-	    var filmstrip_steps = 25;
-	    for (var i = 0; i < prefixes.length; i++) {
-	        var filmstrip = '';
-	        for (var f = 0; f < filmstrip_steps; f++) {
-	            var current_pct = f * (100/filmstrip_steps);
-	            filmstrip += current_pct + '% {background-position:0 -' + (f * 100) + '%;' + prefixes[i] + 'animation-timing-function:steps(1);}';
-	        }
-	        keyframes += '@' + prefixes[i] + 'keyframes filmstrip {' + filmstrip + '}';
-	    }
-	    
-	    var s = document.createElement('style');
-	    s.innerHTML = keyframes;
-	    $('head').append(s);
-	}
+	$('#generate-filmstrip').on('click', function() {
+		// get number of frames
+		var num_frames = $('input').length;
 
-	// var image = new Image();
-	// image.src = '../images/demo/IMG_5130.JPG';
-	// $('.filmstrip').css('background-image', image.src);
+		var image_sources = [];
+		$('input').each(function() {
+			image_sources.push($(this).val());
+		});
+
+		var frames_array = num_frames - 1;
+		var increment = Math.floor(100 / frames_array);
+
+		// create keyframe animation with same number of frames
+		for (var i = 0; i < num_frames; i++) {
+			var frame_percentage = i * increment;
+			if ((i == frames_array) && (frame_percentage !== 100)) {
+				console.log('100% ' + image_sources[i]);
+			} else {
+				console.log(frame_percentage + '% ' + image_sources[i]);
+			}
+		}
+	});
 });
