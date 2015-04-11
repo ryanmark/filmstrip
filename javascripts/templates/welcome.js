b00f589ba8fda31ec771b4685e2b6b4ada624ced
Ryan Mark <ryan@mrk.cc>
2015-04-11 09:20:38 -0400

    updatse

---
+++ javascripts/templates/welcome.js
@@ -0,0 +1,2 @@
+(function() { this.JST || (this.JST = {}); this.JST["templates/welcome"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h2>Hi, ', name ,'!</h2>\n');}return __p.join('');};
+}).call(this);
