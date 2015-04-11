55c736a2a589416ad7dad4edad1d9cacf0c6ef30
Ryan Mark <ryan@mrk.cc>
2015-04-11 11:09:56 -0400

    updates

---
+++ javascripts/templates/alert.js
@@ -0,0 +1,2 @@
+(function() { this.JST || (this.JST = {}); this.JST["templates/alert"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<div data-alert class="alert-box ', type ,'">', msg ,'<a href="#" class="close">&times;</a></div>\n');}return __p.join('');};
+}).call(this);
